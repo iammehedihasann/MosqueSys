6aad3416d65b951303b352d27e8761ed069fd045
iammehedihasann <mehediextrim@gmail.com>
2026-03-12 19:35:08 +0600

    grid update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -92,7 +92,7 @@ export function Home() {
             title="আজকের নামাজের সময়"
             subtitle="আযান ও ইকামতের সঠিক সময়সূচী"
           />
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4 mt-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 mt-8">
             {times.map((prayer) => (
               <PrayerTimeCard key={prayer.id} prayer={prayer} />
             ))}
